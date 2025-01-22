2c90182df8ec78566cab595c13a5ae05678f27b2
AdityaMishra2003 <155647788+AdityaMishra2003@users.noreply.github.com>
2025-01-22 14:50:54 +0530

    Delete src directory

--- src/main/java/com/example/taskmanagement/demo/repository/UserOtpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskmanagement.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.taskmanagement.demo.entity.UserOtp;
-
-public interface UserOtpRepository extends JpaRepository<UserOtp, String> {
-}
